a9112de7191c0f2740a0b30bfe9e4fd163a2de99
ConanVa <gillet.renan@hotmail.fr>
2025-04-21 18:03:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ document
   .getElementById("dateButtonFooter")
   .addEventListener("click", function () {
     var dateList = document.getElementById("dateList");
+    var contactDiv = document.querySelector(".contact"); // Récupère la div contact
+
     // Vérifie si la liste des dates a la classe "hidden"
     if (dateList.classList.contains("hidden")) {
-      dateList.classList.remove("hidden"); // Affiche la liste
-      dateList.style.display = 'block'; // Assure que l'élément soit visible
+      // Affiche la liste des dates
+      dateList.classList.remove("hidden");
+      dateList.style.display = "block"; // Assure que l'élément soit visible
+
+      // Cache la div contact
+      contactDiv.classList.add("hidden");
     } else {
-      dateList.classList.add("hidden"); // Masque la liste
-      dateList.style.display = 'none'; // Assure que l'élément soit masqué
+      // Masque la liste des dates
+      dateList.classList.add("hidden");
+      dateList.style.display = "none"; // Assure que l'élément soit masqué
+
+      // Affiche la div contact
+      contactDiv.classList.remove("hidden");
     }
   });
